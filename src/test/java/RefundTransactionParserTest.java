492c56e110e093c46aabebf46f1baf39b2d9021b
Pierre Smith <smith.pierre@me.com>
2019-03-20 18:27:08 -0400

    changes to trans parser test

--- src/test/java/RefundTransactionParserTest.java
+++ src/test/java/RefundTransactionParserTest.java
@@ -11,28 +11,25 @@ import rapidprocessor.transaction.RefundTransaction;
 import rapidprocessor.transaction.parser.RefundTransactionParser;
 import rapidprocessor.user.User;
 
-
 public class RefundTransactionParserTest {
     @Test
     public void testRefundTransactionParseNone() {
         String line = "01 UUUUUUUUUUUUUUU SSSSSSSSSSSSSSS 000010.00";
-        
-        String ticketTitle = "title",
-        ticketName = "ticketName";
 
-        Integer ticketQty = 10,
-        ticketPrice = 10;
-        
-        RefundTransaction dummyRefund;
+        String ticketTitle = "title", ticketName = "ticketName";
 
+        Integer ticketQty = 10, ticketPrice = 10;
+
+        
         User u = new User("UUUUUUUUUUUUUUS", "AA", 10);
         User s = new User("SSSSSSSSSSSSSSU", "AA", 10);
+        
+        RefundTransaction dummyRefund = new RefundTransaction(null, null, null);
 
         TicketBatch ticket = new TicketBatch(ticketTitle, ticketName, ticketQty, ticketPrice);
-        
+
         List<TicketBatch> tickets = new ArrayList<>();
         List<User> users = new ArrayList<User>();
-        
 
         tickets.add(ticket);
         users.add(u);
@@ -45,19 +42,19 @@ public class RefundTransactionParserTest {
 
     }
 
-    @Test
-    public void testRefundTransactionParseOne() {
-        String line = "01 UUUUUUUUUUUUUUU SSSSSSSSSSSSSSS 000010.00";
-        User u = new User("UUUUUUUUUUUUUUU", "AA", 10);
+    // @Test
+    // public void testRefundTransactionParseOne() {
+    //     String line = "01 UUUUUUUUUUUUUUU SSSSSSSSSSSSSSS 000010.00";
+    //     User u = new User("UUUUUUUUUUUUUUU", "AA", 10);
         
-        List<TicketBatch> tickets = new ArrayList<>();
-        List<User> users = new ArrayList<>();
-        TicketBatch ticket = new TicketBatch(ticketTitle, ticketName, ticketQty, ticketPrice);
+    //     List<TicketBatch> tickets = new ArrayList<>();
+    //     List<User> users = new ArrayList<>();
+    //     TicketBatch ticket = new TicketBatch(ticketTitle, ticketName, ticketQty, ticketPrice);
 
-        tickets.add(ticket);
+    //     tickets.add(ticket);
 
-        RefundTransactionParser parser = new RefundTransactionParser(line, );
+    //     RefundTransactionParser parser = new RefundTransactionParser(line, );
 
 
-    }
+    // }
 }
