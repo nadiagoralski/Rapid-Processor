501ef2952b2a704bfdac4074f56c65aa002185c0
Pierre Smith <smith.pierre@me.com>
2019-03-20 23:49:34 -0400

    merged

--- src/test/java/TicketUtilTest.java
+++ src/test/java/TicketUtilTest.java
@@ -1,14 +1,27 @@
 import org.junit.Test;
 
+import rapidprocessor.ticketBatch.TicketBatch;
+
 public class TicketUtilTest {
+    List<TicketBatch> tickets = new ArrayList<>();
 
     @Test
     public void getTicketBatchData() {
-        
+        TicketBatch tu = new TicketBatch();
+
+        List<TicketBatch>tickets = t.getTicketBatchData();
+
+        this.tickets = tu.getUserData();
+        TicketBatch t = this.tickets.get(0);
+
+        assertEquals("admin", t.getUsername());
+        assertEquals("AA", t.getUserType().getCode());
+        assertEquals("1000.00", t.getUserBalance().toString());
     }
 
     @Test
     public void updateTicketBatch() {
+
     }
 
     @Test
