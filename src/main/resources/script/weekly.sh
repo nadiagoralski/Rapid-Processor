0d499d4fd91ea07d3b7f030d5c0eb6f27a4f7d04
margiepatel <margie.patel@uoit.net>
2019-03-27 18:44:37 -0400

    wording changes

--- src/main/resources/script/weekly.sh
+++ src/main/resources/script/weekly.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-#reset user and tickets files
+#resets the user and tickets files
 cp ../file/out/0users.db users.db;
 cp ../file/out/0tickets.db tickets.db;
 
-#run 5 days
+#runs for 5 days
 for i in (1..5); do
 	./daily.sh $i;
 done
