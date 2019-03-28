c0662c6256e8e9ad35e930a3b3e24f5d9a511791
nadiagoralski <naadialynn@gmail.com>
2019-03-27 23:05:13 -0400

    Script updates

--- src/main/resources/script/daily.sh
+++ src/main/resources/script/daily.sh
@@ -6,7 +6,7 @@ if ["$#" -ne 1]; then
 	echo "Please enter day number"; #Prompts to enter the day number
 else
 	for j in (1..3); do #run 3 sessions
-		./app/pick-it-sales < ../in/$j""input.txt >> /dev/null; #to save output space
+		./app/pick-it-sales < ./app/AvailableUsers.txt ./app/CurrentUsers.txt ../in/$j""input.txt >> /dev/null; #to save output space
 	done #merges
 	cat transactions*.txt > ../out/$i""merge.txt; #saves the merged file
 	rm transactions*;
