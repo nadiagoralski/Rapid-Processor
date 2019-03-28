25527bd9c3b9672f4eab43089fedf105cc9b2c45
margiepatel <margie.patel@uoit.net>
2019-03-27 22:16:08 -0400

    small changes

--- src/main/resources/script/daily.sh
+++ src/main/resources/script/daily.sh
@@ -8,14 +8,14 @@ else
 	for j in (1..3); do #run 3 sessions
 		./ticket-seller < ../input/in$i"_"$j.txt >> /dev/null; #to save output space
 	done #merges
-	cat transactions*.txt > ../printout/$imerge.txt; #saves the merged file
+	cat transactions*.txt > ../printout/merge$i.txt; #saves the merged file
 	rm transactions*;
-	cat ../printout/$imerge.txt > transactions.txt; #overwrites the transaction file
+	cat ../printout/merge$i.txt > transactions.txt; #overwrites the transaction file
 	java Main #runs the backend
 
 	#saves the tickets and accounts files
-	cp users.db ../printout/$1users.db;
-	cp tickets.db ../printout/$1tickets.db;
+	cp users.db ../printout/users$1.db;
+	cp tickets.db ../printout/tickets$1.db;
 
 	echo "Day: "$i" complete";
 
