bcbf4832238f52babe3b3c78e68ecda5e7f50ff6
nadiagoralski <naadialynn@gmail.com>
2019-03-27 21:45:43 -0400

    Updated file paths for scripts

--- src/main/resources/script/daily.sh
+++ src/main/resources/script/daily.sh
@@ -8,14 +8,14 @@ else
 	for j in (1..3); do #run 3 sessions
 		./ticket-seller < ../input/in$i"_"$j.txt >> /dev/null; #to save output space
 	done #merges
-	cat session*.txt > ../file/out/$imerge.txt; #saves the merged file
+	cat session*.txt > ../out/$imerge.txt; #saves the merged file
 	rm session*;
-	cat ../file/out/$imerge.txt > transactions.txt; #overwrites the transaction file
-	java Backend #runs the backend
+	cat ../out/$imerge.txt > transactions.txt; #overwrites the transaction file
+	java Main #runs the backend
 
 	#saves the tickets and accounts files
-	cp users.db ../file/out/$1users.db;
-	cp tickets.db ../file/printout/$1tickets.db;
+	cp users.db ../out/$1users.db;
+	cp tickets.db ../out/$1tickets.db;
 
 	echo "Day: "$i" complete";
 
--- src/main/resources/script/weekly.sh
+++ src/main/resources/script/weekly.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 #resets the user and tickets files
-cp ../file/out/0users.db users.db;
-cp ../file/out/0tickets.db tickets.db;
+cp ../out/0users.db users.db;
+cp ../out/0tickets.db tickets.db;
 
 #runs for 5 days
 for i in (1..5); do
