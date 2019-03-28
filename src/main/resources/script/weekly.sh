bc8fcd114a8f654c66ebbf6f1c24551e6cda022e
nadiagoralski <naadialynn@gmail.com>
2019-03-27 23:25:15 -0400

    Script updates

--- src/main/resources/script/weekly.sh
+++ src/main/resources/script/weekly.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 #resets the user and tickets files
-cp ../out/0users.db users.db;
-cp ../out/0tickets.db tickets.db;
+cp ../out/0users.db ../file/users.db;
+cp ../out/0tickets.db ../filetickets.db;
 
 #runs for 5 days
 for i in (1..5); do
